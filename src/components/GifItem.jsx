63c1a86267764d7459365a051a622aea598330ed
Oscar Vasquez <oscareduc14@hotmail.com>
2023-09-05 11:13:46 -0500

    eliminacion del title en el gif

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,6 @@ export const GifItem = ({title, url, id}) => {
     return (
         <div className="card">
             <img src={url} alt={title} />
-            <p>{title}</p>
         </div>
     )
 }
